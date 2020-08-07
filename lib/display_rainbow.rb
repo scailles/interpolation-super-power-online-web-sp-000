e6c6c006047570e68c6de9698f0c96d710aa92d2
Shaina C <scailles23@gmail.com>
2020-08-07 02:35:55 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,6 +4,6 @@
 rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 def display_rainbow(rainbow)
-  puts "R: #{rainbow[0]}"
+  puts "R: #{rainbow[0]}, O: #{rainbow[1]}, 
 end
   
